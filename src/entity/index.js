c93a3d2578dcd53147a984add2c106f8f57e5c7d
itz-champion <sainiabhijot@hotmail.com>
2024-12-29 00:12:08 -0500

    Delete src directory

--- src/entity/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    Cell: require('./Cell'),
-    PlayerCell: require('./PlayerCell'),
-    Food: require('./Food'),
-    Virus: require('./Virus'),
-    MotherCell: require('./MotherCell'),
-    EjectedMass: require('./EjectedMass'),
-};
